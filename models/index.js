c443d782dbc3ea6ae685fdb132254a8b4ac280a3
Adjoa Hackman <adjoahackman93@gmail.com>
2024-06-08 17:59:35 -0400

    pushing more changes

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const User = require('./User');
 const Thought = require('./Thought');
 const Reaction = require('./Reaction');
 
-
+//do I need to require the Reaction model?
 module.exports = {
     User, Thought, Reaction 
-}
+};
